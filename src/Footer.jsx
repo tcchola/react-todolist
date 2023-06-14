e5fb4482f160f4bccc972575356c1f522bec2014
tcchola <tarikcolic22@gmail.com>
2023-06-14 19:39:05 +0200

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer(){
+    return(
+        <>
+        <div className="footer">
+            by <a href="https://www.github.com/tcchola">@tcchola</a>
+        </div>
+        </>
+    )
+}
